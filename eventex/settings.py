97e1a6c88c5e637b3e571d40a32483634ef0f59e
Eduardo <eduardoshibuka@gmail.com>
2018-04-26 18:37:08 +0000

    Persiste a inscrição

--- eventex/settings.py
+++ eventex/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'test_without_migrations',
     'django_extensions',
+
     'eventex.core',
     'eventex.subscriptions'
 )
